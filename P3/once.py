305cf021d35eff62a51109f29fe5608d95e3673f
bellota56789 <lemosavellana@gmail.com>
2026-05-27 14:51:56 -0600

    Add files via upload

---
+++ P3/once.py
@@ -0,0 +1,9 @@
+class Public:
+    
+    def __init__(self, user_id, time_stamp):
+        self.user_id = id 
+        self.time_stamp = time_stamp      
+
+    
+    def resultado(self):
+        return f"{self.user_id} dice: obtener resultado"

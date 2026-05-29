5190790987a8767a35fa189de451b7b77a960206
bellota56789 <lemosavellana@gmail.com>
2026-05-29 17:02:20 -0600

    Add files via upload

---
+++ P3/nueve.py
@@ -0,0 +1,23 @@
+create table stadium (
+id int auto_increment NOT NULL,
+visit_date DATE NOT NULL,
+people int NOT NULL,
+);
+
+insert into stadium(id, visit_date, people)
+VALUES(1, '2017-01-01', 10),
+(2, '2017-01-02', 109),
+(3, '2017-01-03', 150),
+(4, '2017-01-04', 99),
+(5, '2017-01-05', 145),
+(6, '2017-01-06', 1455),
+(7, '2017-01-07', 199),
+(8, '2017-01-08', 188);
+
+SELECT 5, AS '2017-01-05' AS 10
+UNION ALL
+6, '2017-01-06', 1455
+UNION ALL 
+7, '2017-01.07', 199
+UNION ALL
+8, '2017-01-08', 188
